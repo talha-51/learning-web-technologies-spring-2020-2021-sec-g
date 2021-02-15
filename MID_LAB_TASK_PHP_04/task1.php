d9de414024ca0b9575d4edc3ec84b566b48cb00c
talha-51 <mahbuburrahmantalha@gmail.com>
2021-02-15 20:48:32 +0600

    task 1

---
+++ MID_LAB_TASK_PHP_04/task1.php
@@ -0,0 +1,13 @@
+<?php
+
+    $length = 10;
+    $width =5;
+    $area =$length * $width;
+    $perimeter=2 * ($length + $width);
+    
+    echo"Length of the Rectangle: $length \n";
+    echo"Width of the Rectangle: $width \n";
+    echo"Area of the Rectangle: $area \n";
+    echo"Perimeter of the Rectangle: $perimeter";
+
+?>
