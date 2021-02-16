0ed4d7cf763f3e3d4bc9046977e687938c193273
talha-51 <mahbuburrahmantalha@gmail.com>
2021-02-16 11:10:55 +0600

    task3 taskC

---
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_C/myname.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DOB Input field</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend>DATE OF BIRTH</legend>
+			<input type="date" name="dob" value="<?php if(isset($_POST['dob'])){ echo $_POST['dob']; }?>"> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
