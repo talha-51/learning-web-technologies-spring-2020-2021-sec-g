ba6a01938195c10478ec61b3a486b3a0999d3e41
talha-51 <mahbuburrahmantalha@gmail.com>
2021-02-16 11:07:45 +0600

    task2 taskA

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_A/name.php
@@ -0,0 +1,41 @@
+<?php
+
+	//echo "test";
+	//$array = [1, 'alamin', 'alamin@aiub.edu'];
+	//$asso = ['id'=>1, 'name'=>'alamin', 'email'=>'alamin@aiub.edu'];
+	//print_r($_SERVER);
+	//phpinfo();
+	//print_r($_ENV);
+
+	/*$x = 10;
+	$y = 20;
+
+	function sum(){
+		//return $GLOBALS['x']+$GLOBALS['y'];
+
+		global $x;
+		global $y;
+		return $x+$y;
+	}
+
+	echo sum();*/
+
+	//print_r($_GET);
+	
+
+	if(isset($_POST['submit'])){
+
+		$email 		=  $_POST['email'];
+
+		if($email == ""){
+			echo "Null submission...";
+		}else{
+			echo "Success";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?>
